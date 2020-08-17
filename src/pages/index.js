fdef8c2c1f97756751197756a1844c7b1f38f65f
Satyajeet Maharana <sm8235@nyu.edu>
2020-08-16 22:28:47 -0400

    Updated site images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Project from "../components/project"
+import SEO from "../components/seo"
 
 
 // import {
@@ -15,7 +16,9 @@ import { Router } from "@reach/router";
 
 const IndexPage = () => (
   <div>
+    <SEO title="Home" />
     <Router>
+        
         <Layout path="/"/>
         <Project path="/project/:projectURL"/>
         {/* <Route exact path="/about" component={AboutPage} /> */}
